df4f507cda0c235775f4c960cd983f41d4ef824c
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-10 15:48:24 -0500

    created todoReducer.js created initState (default todo list)

---
+++ dorothy/src/reducers/todoReducer.js
@@ -0,0 +1,27 @@
+//reducer to be used in: 
+export const reducer = (state, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
+
+
+//initial todo list
+export const initState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Get through this lesson this time', 
+        completed: false,
+        id: 3892987590
+    },
+    {
+        item: 'Try to hit stretch as well', 
+        completed: false,
+        id: 3892987591
+    }
+]
